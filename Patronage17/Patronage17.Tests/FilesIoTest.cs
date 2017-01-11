295e1e746c07c5ae54d30e7874bc37534acba907
lsielewicz <lsielewicz8@gmail.com>
2017-01-11 18:29:34 +0100

    Task 2 - Part 2: Added unit tests for GetMetadata method

--- Patronage17/Patronage17.Tests/FilesIoTest.cs
+++ Patronage17/Patronage17.Tests/FilesIoTest.cs
@@ -1,7 +1,10 @@
 ﻿using Patronage17.Engine.Helpers;
+using Patronage17.Engine.Interfaces;
 using Patronage17.Tests.Helpers;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace Patronage17.Tests
@@ -45,6 +48,33 @@ namespace Patronage17.Tests
             Assert.False(Directory.Exists(LocalizationStringsProvider.NotExistingPath));
         }
 
+        [Fact]
+        public void TryToExecuteGetFilesWithIncorrectArgument()
+        {
+            Assert.Null(FilesIoHelper.Instance.GetFilesFromDirectory(null));
+        }
 
+        [Fact]
+        public void GetMetataOfEveryFileInApplicationHomeDirectory()
+        {
+            var filePathes = FilesIoHelper.Instance.GetFiles(LocalizationStringsProvider.ApplicationLocalization);
+            Assert.NotNull(filePathes);
+            Assert.NotEmpty(filePathes);
+
+            var fileInfos = new List<IFileInfo>();
+            filePathes.ToList().ForEach(filePath =>
+            {
+                fileInfos.Add(FilesIoHelper.Instance.GetFileMetadata(filePath));
+            });
+
+            Assert.NotEmpty(fileInfos);
+        }
+
+        [Fact]
+        public void GetMetadataOfNotExistingFile()
+        {
+            var file = FilesIoHelper.Instance.GetFileMetadata(Path.Combine(LocalizationStringsProvider.ApplicationLocalization, "NotExistringFile.exe"));
+            Assert.Null(file);
+        }
     }
 }
