9d4f2652d10b1dd438bd74a35d005ecd02a9ee2a
lsielewicz <lsielewicz8@gmail.com>
2017-01-19 19:18:18 +0100

    Added simple rest api and simple webpagelike mechanism

---
+++ Patronage17/Patronage17.Web/Models/HomeViewModels/FilesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Patronage17.Engine.Interfaces;
+using System.Collections.Generic;
+
+namespace Patronage17.Web.Models.HomeViewModels
+{
+    public class FilesViewModel
+    {
+        public string Directory { get; set; }
+        public Dictionary<string, IFileInfo> Files { get; set; }
+        
+        public FilesViewModel()
+        {
+            this.Files = new Dictionary<string, IFileInfo>();
+        }
+    }
+}
--- Patronage17/Patronage17.Web/Startup.cs
+++ Patronage17/Patronage17.Web/Startup.cs
@@ -10,60 +10,33 @@ using Microsoft.Extensions.Logging;
 using System.Reflection;
 using Patronage17.Engine.Helpers;
 using Microsoft.AspNetCore.Routing;
-using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace Patronage17.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRouting();
+            services.AddMvc();
+            services.AddSingleton<FilesIoHelper>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            var routeBuilder = new RouteBuilder(app);
-            var appLocation = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-
-            routeBuilder.MapGet("", context => 
+            app.UseMvc(routes =>
             {
-                var files = FilesIoHelper.Instance.GetFiles(appLocation);
-                var output = string.Join(Environment.NewLine, files);
-
-                return context.Response.WriteAsync(output);
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
-
-            routeBuilder.MapGet("{fileName}", context =>
-            {
-                var fileName = context.GetRouteValue("fileName").ToString();
-                var filePath = Path.Combine(appLocation, fileName);
-                var fileInfo = FilesIoHelper.Instance.GetFileMetadata(filePath);
-
-                if (fileInfo != null)
-                {
-                    var output = $"{"File name:",-25} {fileInfo.Name}{Environment.NewLine}" +
-                                 $"{"Extension:",-25} {fileInfo.Extension}{Environment.NewLine}" +
-                                 $"{"Last write time:",-25} {fileInfo.LastWriteTime}{Environment.NewLine}" +
-                                 $"{"Creation time:",-25} {fileInfo.CreationTime}{Environment.NewLine}";
-
-                    return context.Response.WriteAsync(output);
-                }
-                return context.Response.WriteAsync($"Couldn't get the information about {context.GetRouteValue("fileName")}");
-            });
-        
-
-            app.UseRouter(routeBuilder.Build());
 
         }
     }
